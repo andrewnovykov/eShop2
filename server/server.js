7cd989ec7e1dcda9c03c29f41984d38ea6682674
Anrew <papapin777@gmail.com>
2020-10-07 20:54:47 -0700

    initial setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, ()=>{
+    
+    console.log(`Server runing on port: ${port}`);
+
+})
+
